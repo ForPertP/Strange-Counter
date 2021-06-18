4826886c545b30df0f3ef4f87401ed01f755d9c1
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2021-06-19 01:15:57 +0900

    Update Strange-Counter.cpp

--- Strange-Counter.cpp
+++ Strange-Counter.cpp
@@ -10,3 +10,16 @@ long strangeCounter(long t)
 
     return result - t + 1;
 }
+
+
+long strangeCounter1(long t)
+{
+    long result = 4;
+
+    while (t >= result)
+    {
+        result = result * 2 + 2;
+    }
+    
+    return result - t;
+}
