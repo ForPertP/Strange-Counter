95167039f9ba0b0fb321c94455f044060ec671ab
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-10-24 18:58:09 +0900

    Update Strange-Counter.cpp

--- Strange-Counter.cpp
+++ Strange-Counter.cpp
@@ -37,3 +37,22 @@ long strangeCounter1(long t)
     
     return result - t;
 }
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string t_temp;
+    getline(cin, t_temp);
+
+    long t = stol(ltrim(rtrim(t_temp)));
+
+    long result = strangeCounter(t);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
