c2d86ede48031fe8f4d6137dd1e45bef2424570e
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-10-27 01:11:11 +0900

    Create Strange-Counter.cs

---
+++ Strange-Counter.cs
@@ -0,0 +1,24 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'strangeCounter' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts LONG_INTEGER t as parameter.
+     */
+}
