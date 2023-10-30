d33db9bac9425d539aef479352ff7705f2bf754f
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2023-10-30 23:06:52 +0900

    Update Strange-Counter.java

--- Strange-Counter.java
+++ Strange-Counter.java
@@ -14,3 +14,21 @@ class Result {
 
  
 }
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        long t = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.strangeCounter(t);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
